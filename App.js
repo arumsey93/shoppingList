3385545836ef5fda63ff5963c12b7744af13c176
Alex rumsey <alexrumsey1993@gmail.com>
2020-07-17 15:24:31 -0500

    finished small app with crud functionality

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import UUIDGenerator from 'react-native-uuid-generator';
+import Header from './components/header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: UUIDGenerator.getRandomUUID(), text: 'Milk'},
+    {id: UUIDGenerator.getRandomUUID(), text: 'Eggs'},
+    {id: UUIDGenerator.getRandomUUID(), text: 'Bread'},
+    {id: UUIDGenerator.getRandomUUID(), text: 'Juice'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'OK'});
+    } else {
+      setItems((prevItems) => {
+        return [{id: UUIDGenerator.getRandomUUID(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
 
 export default App;
